32790d9cd5c638e7a2f88f5a07c7b33ecdca3386
yigitortu <yigittortu287@gmail.com>
2023-12-10 11:17:25 +0300

    Patika ilk repository

---
+++ ilkrepo/src/patikailkrepo/firstpushforgithub.java
@@ -0,0 +1,8 @@
+package patikailkrepo;
+
+public class firstpushforgithub {
+    public static void main (String[] args){
+        System.out.print("Patika.dev FullStack Java Spring boot & React Bootcamp");
+    }
+
+}
